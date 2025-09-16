eb3112ca9e0d9c79ef218b8fd9af239ff8686034
basma21trid-cpu <basma21trid@gmail.com>
2025-09-16 17:23:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
- for (let i = 1; i <= 10; i++) {
-    if ( i === 5) continue;
-    console.log("Nombre : " + i );
- }
+/*Variables */
+let nombre1 = 10;
+let nombre2 = 5;
+let somme = nombre1 + nombre2;
+console.log("La somme est : " + somme);
+
+/*Fonctions */
+function addition(a, b) {
+  return a + b;
+}
+
+let somme = addition(8, 5);
+console.log("La somme est : " + somme);
+
+/*conditions */
+let age = 20;
+
+if (age < 18) {
+  console.log("Vous êtes mineur.");
+} else if (age === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
+
+
+/*Boucles */
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) continue; // sauter le 5
+  console.log("Nombre : " + i);
+}
+
