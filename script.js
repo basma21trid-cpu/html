d7776c41d3480f7fef88a959d83c64730e6fe10d
basma21trid-cpu <basma21trid@gmail.com>
2025-09-16 14:36:11 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let nombre1 = 10;
+let nombre2 = 5;
+let somme = nombre1 + nombre2;
+console.log("La somme est : " + somme);
+function addition(a, b) {
+  return a + b;
+}
+
+let somme = addition(8, 5);
+console.log("La somme est : " + somme);
+
 let age = 20;
 if (age < 18) {
     console.log (" Vous êtes mineur." );
@@ -5,4 +16,5 @@ if (age < 18) {
     console.log ("vous venez d'atteindre la majorité !");
         } else {
             console.log(" Vous êtes majeur.");      
+
          }
