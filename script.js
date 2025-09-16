738f9a1f6887f0da4af613c54ac8794daafce696
basma21trid-cpu <basma21trid@gmail.com>
2025-09-16 14:25:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function addition(a , b) {
-    return a + b;
-}
+let age = 20;
 
-let somme = addition (8, 5);
-console.log("La somme est :" + somme);
+if (age < 18) {
+    console.log (" Vous êtes mineur." );
+}    else if ( age ===18) {
+    console.log ("vous venez d'atteindre la majorité !");
+        } else {
+            console.log(" Vous êtes majeur.");      
+         }
